3e7e5af3ea22d6b9ff34e200c02179aaf41a2935
atogz <webdevatogz@gmail.co>
2020-02-04 01:27:38 +0300

    tasks add + delete

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -78,16 +78,28 @@
     <transition name="fade" mode="out in">
       <div class="w-full" v-if="activeScreen === 'tasks'">
         <div class="w-full flex flex-col" v-if="getProjectData.tasks.length">
-          <div class="w-full flex py-5 px-5">
-            <p>Задач: {{ getProjectData.tasks.length }}</p>
-            <p class="ml-5">
-              Выполнено: {{ getCompletedTasks.length }} <b>({{ progress }}%)</b>
-            </p>
+          <div class="w-full flex items-center">
+            <div class="w-2/3 flex py-5 px-5">
+              <p>Задач: {{ getProjectData.tasks.length }}</p>
+              <p class="ml-5">
+                Выполнено: {{ getCompletedTasks.length }}
+                <b>({{ progress }}%)</b>
+              </p>
+            </div>
+            <div class="w-1/3">
+              <button
+                @click="addTask()"
+                class="ml-auto mr-10 py-2 px-2 border-2 border-green-400 bg-green-400 text-white rounded flex items-center justify-center hover:bg-green-600 "
+              >
+                добавить задачу
+              </button>
+            </div>
           </div>
+
           <div class="w-full mt-5 border-t-2 py-5 px-5">
             <div
               class="w-full py-5 px-5 mt-5 rounded overflow-hidden shadow-lg cursor-pointer border-2 border-gray-200 flex mt-5"
-              v-for="task in getProjectData.tasks"
+              v-for="(task, index) in getProjectData.tasks"
               :key="task.id"
             >
               <div class="w-full flex items-center justify-center">
@@ -104,21 +116,32 @@
                   >
                     {{ task.completed ? "Выполнена" : "Не выполнена" }}
                   </p>
+                  <p class="mt-5 text-sm text-gray-500">
+                    <i>Дедлайн:</i> <b>{{ task.deadline }}</b>
+                  </p>
                 </div>
                 <div
                   class="w-3/5 ml-10 text-sm text-gray-600 px-5 text-justify"
                 >
                   <p>{{ task.description }}</p>
                 </div>
-                <button
-                  @click="
-                    (task.completed = !task.completed),
-                      changeTaskStatus(task.completed)
-                  "
-                  class="w-1/5 ml-20 py-5 px-5 border-2 border-gray-500 bg-gray-500 text-white rounded flex items-center justify-center hover:bg-gray-700 "
-                >
-                  изменить статус
-                </button>
+                <div class="w-1/5 flex flex-col">
+                  <button
+                    @click="
+                      (task.completed = !task.completed),
+                        changeTaskStatus(task.completed)
+                    "
+                    class="w-full py-5 px-5 border-2 border-gray-500 bg-gray-500 text-white rounded flex items-center justify-center hover:bg-gray-700 "
+                  >
+                    изменить статус
+                  </button>
+                  <button
+                    @click="deleteTask(index)"
+                    class="w-full mt-5 py-5 px-5 border-2 border-red-500 bg-red-500 text-white rounded flex items-center justify-center hover:bg-red-700 "
+                  >
+                    удалить
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -126,6 +149,12 @@
         <div class="w-full flex flex-col" v-else>
           <div class="w-full flex py-5 px-5">
             <p>Задач пока не добавлено</p>
+            <button
+              @click="addTask()"
+              class="ml-auto mr-10 py-2 px-2 border-2 border-green-400 bg-green-400 text-white rounded flex items-center justify-center hover:bg-green-600 "
+            >
+              добавить задачу
+            </button>
           </div>
         </div>
       </div>
@@ -153,7 +182,7 @@
             <div class="w-1/3">
               <button
                 @click="addMaterial()"
-                class="float-right py-2 px-2 border-2 border-gray-500 bg-gray-500 text-white rounded flex items-center justify-center hover:bg-gray-700 "
+                class="float-right py-2 px-2 border-2 border-green-400 bg-green-400 text-white rounded flex items-center justify-center hover:bg-green-600 "
               >
                 добавить материал
               </button>
@@ -216,8 +245,14 @@
         </div>
 
         <div class="w-full flex flex-col" v-else>
-          <div class="w-full flex py-5 px-5">
+          <div class="w-full flex py-5 px-5 items-center">
             <p>Материалов пока не добавлено</p>
+            <button
+              @click="addMaterial()"
+              class="ml-auto mr-10 py-2 px-2 border-2 border-green-400 bg-green-400 text-white rounded flex items-center justify-center hover:bg-green-600 "
+            >
+              добавить материал
+            </button>
           </div>
         </div>
       </div>
@@ -248,6 +283,42 @@ export default {
     };
   },
   methods: {
+    addTask() {
+      let itemOne = {
+        id: 4324,
+        name: "Задача 4324",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat sem at risus molestie, vitae volutpat mauris viverra. Proin maximus cursus est. Donec vulputate turpis sit amet quam aliquet, id ultricies orci tempor. Praesent in libero malesuada, mollis lacus vitae, ornare orci. Fusce ac luctus diam, nec ornare nunc. Fusce a eros consectetur, placerat erat vitae, facilisis ligula. Quisque dignissim, ipsum eget porttitor gravida, risus dolor fermentum enim, ac tempus elit dui non justo. Nulla vestibulum lobortis vehicula. In ullamcorper elementum ante, quis ultricies felis dapibus ut. Aliquam erat volutpat. Quisque in commodo massa. Duis et efficitur velit. Nunc sed ex ultricies, tristique justo et, egestas orci. Morbi molestie ante ut augue tempor hendrerit.",
+        deadline: "22.03.2020",
+        completed: false
+      };
+      let itemTwo = {
+        id: 5212,
+        name: "Задача 5212",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat sem at risus molestie, vitae volutpat mauris viverra. Proin maximus cursus est. Donec vulputate turpis sit amet quam aliquet, id ultricies orci tempor. Praesent in libero malesuada, mollis lacus vitae, ornare orci. Fusce ac luctus diam, nec ornare nunc. Fusce a eros consectetur, placerat erat vitae, facilisis ligula. Quisque dignissim, ipsum eget porttitor gravida, risus dolor fermentum enim, ac tempus elit dui non justo. Nulla vestibulum lobortis vehicula. In ullamcorper elementum ante, quis ultricies felis dapibus ut. Aliquam erat volutpat. Quisque in commodo massa. Duis et efficitur velit. Nunc sed ex ultricies, tristique justo et, egestas orci. Morbi molestie ante ut augue tempor hendrerit.",
+        deadline: "22.03.2020",
+        completed: false
+      };
+      let itemThree = {
+        id: 312312,
+        name: "Задача 32131",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat sem at risus molestie, vitae volutpat mauris viverra. Proin maximus cursus est. Donec vulputate turpis sit amet quam aliquet, id ultricies orci tempor. Praesent in libero malesuada, mollis lacus vitae, ornare orci. Fusce ac luctus diam, nec ornare nunc. Fusce a eros consectetur, placerat erat vitae, facilisis ligula. Quisque dignissim, ipsum eget porttitor gravida, risus dolor fermentum enim, ac tempus elit dui non justo. Nulla vestibulum lobortis vehicula. In ullamcorper elementum ante, quis ultricies felis dapibus ut. Aliquam erat volutpat. Quisque in commodo massa. Duis et efficitur velit. Nunc sed ex ultricies, tristique justo et, egestas orci. Morbi molestie ante ut augue tempor hendrerit.",
+        deadline: "22.03.2020",
+        completed: false
+      };
+
+      let randomPick = Math.floor(Math.random() * 3);
+
+      if (randomPick === 0) {
+        return this.getProjectData.tasks.unshift(itemOne);
+      } else if (randomPick === 1) {
+        return this.getProjectData.tasks.unshift(itemTwo);
+      } else {
+        return this.getProjectData.tasks.unshift(itemThree);
+      }
+    },
     changeTaskStatus(status) {
       console.log("status ", status);
       if (status) {
@@ -256,6 +327,10 @@ export default {
         this.project.completedTasks--;
       }
     },
+    deleteTask(index) {
+      return this.getProjectData.tasks.splice(index, 1);
+    },
+
     addMaterial() {
       let itemOne = {
         id: 51,
